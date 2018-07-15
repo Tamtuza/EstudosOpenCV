2245a2dcee020410e8357229cd5253a79c079389
Tamillys Pantuza <tamillyspantuza@gmail.com>
2018-07-15 07:00:19 -0300

    Adicionando comentários explicando o que foi feito nos últimos exemplos

--- exemploImagens/exemplo3/terceiroExemplo.py
+++ exemploImagens/exemplo3/terceiroExemplo.py
@@ -4,7 +4,13 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread("frida.jpg", 0)
 
+#Ploto o histograma da imagem
+#Ravel transforma a imagem (matriz 2D) em um vetor de uma dimensão
+#256 é a representação do tom de cor no ponto x (bins)
+#[0, 256] de que ponto em x até que ponto em x vou representar (intervalo do histograma)
 plt.hist(img.ravel(), 256, [0, 256])
+
+#Mostra na tela a imagem e o histograma
 cv2.imshow("Imagem Original", img)
 plt.show()
 
--- exemploImagens/exemplo4/quartoExemplo.py
+++ exemploImagens/exemplo4/quartoExemplo.py
@@ -4,12 +4,23 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread("alanis.jpg")
 
+#Cria uma lista contendo os canais de cores para construir
+#o histograma dos três canais
 color = ('b', 'g', 'r')
 
+#Itera entre os canais BGR
 for i, col in enumerate(color):
+    
     #print (i, col)
+
+    #Gera o histograma usando: a imagem, o identificador do canal,
+    #a máscara (caso queira calcular apenas uma região), o bin e o intervalo
     histr = cv2.calcHist([img], [i], None, [256], [0, 256])
+
+    #Constrói o histograma para o canal de cor específico
     plt.plot(histr, color = col)
+
+    #Define os limites do histograma em relação a x (igual ao intervalo acima)
     plt.xlim([0, 256])
 
 cv2.imshow("ImagemOriginal", img)
